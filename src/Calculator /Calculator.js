0656ad4528e841bc2f4dcdc33c52504f9921e84e
Akira McPhee <akiramcphee@gmail.com>
2022-07-06 12:05:08 +1000

    calc-inputs

--- src/Calculator /Calculator.js
+++ src/Calculator /Calculator.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import CalculatorForm from './CalculatorForm /CalculatorForm'
+import './Calculator.css'
 
 export default function Calculator() {
   return (
     <div>
-      <div>This is test Calculator</div>
-      <CalculatorForm/>
+      <div className = 'calc-inputs'>
+        <div id='assets-calc'>
+          <CalculatorForm name = 'Assets'/>
+        </div>
+        <div id = 'liabilities-calc'>
+          <CalculatorForm name = 'Liabilities'/>
+        </div>
+        <div id = 'income-calc'>
+          <CalculatorForm name = 'Income'/>
+        </div>
+      </div>
     </div>
 
   )
--- src/Calculator /CalculatorForm /CalculatorForm.js
+++ src/Calculator /CalculatorForm /CalculatorForm.js
@@ -1,14 +1,49 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react';
 
-export default function CalculatorForm() {
+export default function CalculatorForm(props) {
+	const [currentSum,setCurrentSum]=useState(0);
+  const [clear,setClear]=useState(false);
+
+  useEffect(()=>{
+    document.querySelector('#result').value="";
+  },[])
+  
+  useEffect(()=>{
+    if(clear)
+    document.querySelector('#result').value="";
+  })
+
+  const Add=(e)=>{
+    e.preventDefault();
+    if(clear) setClear(false);
+    let currentNum=document.querySelector('#num').value
+    if(currentNum=='')
+    return;
+    let sum= currentSum+parseInt(currentNum);
+    setCurrentSum(sum);
+    document.querySelector('#num').value="";
+      
+  }
+
+  const Clear=(e)=>{
+    e.preventDefault();
+    console.log('sum:', currentSum);
+    document.querySelector('form').reset();
+    setClear(true);
+    setCurrentSum(0);
+  }
+
   return (
-    <div className='CalcForm'>
-        <div className='CalcInput'>
-            <form>
-                <label for="fname">Assets:</label>
-                <input type="text" id="fname" name="fname"/>
-            </form>
-        </div>
-    </div>
+		<div className="Calc">
+		<div className="Calc-title">
+			<h1> {props.name} </h1>
+		</div>
+		<form>
+					<input type="text" id="result" value={currentSum} placeholder="sum" readOnly />   
+					<input type="text" id="num" placeholder="enter a number" />
+					<button onClick={Add}>Add</button>
+					<button onClick={Clear}>Clear</button>
+		</form>
+	</div>
   )
 }
