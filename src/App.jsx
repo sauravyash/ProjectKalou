5e5f1ea81b7f49f0617df0820061e2a78ea05678
sauravyash <12910821+sauravyash@users.noreply.github.com>
2022-07-08 17:27:52 +1000

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { faker } from '@faker-js/faker';
 import 'bulma/css/bulma.min.css';
 
 import Navbar from './Components/Navbar';
@@ -8,8 +9,28 @@ import Table from './Components/Table';
 
 import './App.css';
 
+const sampleArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
 function App() {
   const [userData, setUserData] = React.useState([]);
+
+  const uD = {
+    labels: sampleArray.map((a) => {
+      const d = new Date(Date.now());
+      d.setMonth(d.getMonth() + a);
+      return new Intl.DateTimeFormat('en-US', {
+        month: 'short',
+      }).format(d);
+    }),
+    datasets: [
+      {
+        label: 'Sample Data',
+        data: sampleArray.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+    ],
+  };
 
   return (
     <div className="App columns is-flex-direction-column m-0 p-4  is-align-items-center is-justify-content-center">
@@ -18,7 +39,7 @@ function App() {
         <Calculator userData={userData} setUserData={setUserData} />
         <Table />
       </div>
-      <Display className="column" userData={userData} />
+      <Display className="column" chartData={uD} />
     </div>
   );
 }
--- src/Components/Display/index.jsx
+++ src/Components/Display/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-
-import { faker } from '@faker-js/faker';
+import PropTypes from 'prop-types';
 
 import {
   Chart as ChartJS,
@@ -38,32 +37,24 @@ export const options = {
   },
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-export const chartData = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: 'Dataset 2',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-  ],
-};
-
-export default function Display() {
+export default function Display(props) {
+  const { chartData } = props;
   return (
-    <div className="">
+    <div className="container" style={{ width: '100%' }}>
       <ChartContainer className="column">
         <Chart className="p-5" type="line" options={options} data={chartData} />
       </ChartContainer>
     </div>
   );
 }
+
+Display.propTypes = {
+  chartData: PropTypes.arrayOf({
+    date: PropTypes.number,
+    amount: PropTypes.number,
+  }),
+};
+
+Display.defaultProps = {
+  chartData: [],
+};
